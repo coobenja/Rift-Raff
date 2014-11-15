1e4b3ee82f95e00ee47bd6a30d9c86b09a92201e
gatriex <haasjac@gmail.com>
2014-11-15 10:51:09 -0500

    Added BackgroundControl Script
    Changes Background image with left and right arrow keys.

---
+++ Battle_Ted Unity/Assets/backgroundControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundControl : MonoBehaviour {
+	public float X;
+	public float W;
+	public string direction;
+	public float magnitude;
+	//public GameObject leftplayer;
+	//public GameObject rightplayer;
+	// Use this for initialization
+	void Start () {
+		camera.rect = new Rect(X,0,W,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			if (direction == "Left") {
+				W = W + magnitude;
+			} else {
+				W = W - magnitude;
+				X = magnitude + X;
+			}
+			camera.rect = new Rect(X,0,W,1);
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			if (direction == "Left") {
+				W = W - magnitude;
+			} else {
+				W = W + magnitude;
+				X = -magnitude + X;
+			}
+			camera.rect = new Rect(X,0,W,1);
+		}
+	}
+}
