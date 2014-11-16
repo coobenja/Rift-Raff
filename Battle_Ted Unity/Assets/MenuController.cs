05e718cd4f1acf33944e67a2c5c2b7d2a3945604
gatriex <haasjac@gmail.com>
2014-11-16 11:23:57 -0500

    Menu for Real

---
+++ Battle_Ted Unity/Assets/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	private Animator animator;
+	private int page;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		page = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("start") && page == 0) {
+			print ("one");
+			animator.SetInteger ("page", 1);
+			page = 1;
+		}
+		if (Input.GetButtonDown("Jump_1") && page == 1) {
+			animator.SetInteger ("page", 2);
+			page = 2;
+		} 
+		else if (Input.GetButtonDown("Jump_1") && page == 2) {
+			Application.LoadLevel("backgroundtest");
+		}
+	}
+}
