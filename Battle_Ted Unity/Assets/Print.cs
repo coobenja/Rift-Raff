9dbab1aa84353fa2aec7b1b833cae507bd187fff
gatriex <haasjac@gmail.com>
2014-11-16 11:05:01 -0500

    Menu Start

---
+++ Battle_Ted Unity/Assets/Print.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Print : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (Screen.width);
+		print (Screen.height);
+	}
+}
